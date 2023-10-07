fc7b6031068479e0f22ea28d49cda82d51e6eab0
hvt16 <harshvardhantailor9@gmail.com>
2023-10-07 21:04:33 +0530

    abcd

--- DataColl/settings.py
+++ DataColl/settings.py
@@ -110,3 +110,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+CSRF_TRUSTED_ORIGINS = ['*']
